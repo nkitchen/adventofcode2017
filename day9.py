091b2c1d71ae5345d361e8b5da6461e208ba9c78
Nathan Kitchen <nkitchen@aristanetworks.com>
2016-12-09 10:50:23 -0800

    Day 9, part 2

--- day9.py
+++ day9.py
@@ -3,24 +3,25 @@
 import re
 import sys
 
-input = sys.stdin.read().strip()
-
 markerRe = re.compile(r"[(](\d+)x(\d+)[)]")
 
-i = 0
-n = 0
-while i < len(input):
-    m = markerRe.search(input, i)
-    if not m:
-        n += len(input) - i
-        break
+def decompressedLen(input):
+    i = 0
+    n = 0
+    while i < len(input):
+        m = markerRe.search(input, i)
+        if not m:
+            n += len(input) - i
+            break
 
-    j = m.start()
-    n += j - i
-    chars = int(m.group(1))
-    reps = int(m.group(2))
-    n += chars * reps
-    i = m.end() + chars
+        j = m.start()
+        n += j - i
+        chars = int(m.group(1))
+        reps = int(m.group(2))
+        repeated = input[m.end() : m.end() + chars]
+        n += reps * decompressedLen(repeated)
+        i = m.end() + chars
 
-print n
+    return n
 
+print decompressedLen(sys.stdin.read().strip())
