74fc059aff80917510fddbd2f3cdbf21b2b58c6a
Nathan Kitchen <nkitchen@aristanetworks.com>
2016-12-22 09:34:23 -0800

    Day 20, part 2: try 1

--- day20.py
+++ day20.py
@@ -1,5 +1,12 @@
 import fileinput
+import os
 import re
+
+M = os.environ.get("MAX")
+if M:
+   M = int(M)
+else:
+   M = 2 ** 32
 
 blocks = []
 
@@ -12,8 +19,20 @@ for line in fileinput.input():
 
 blocks.sort()
 
-for a, b in blocks:
-   x = b + 1
-   if not any(c <= x <= d for c, d in blocks):
-      print x
-      break
+n = 0
+a = blocks[0][0]
+if a > 0 :
+   n += a
+
+for i in xrange(len(blocks) - 1):
+   b = blocks[i][1]
+   a = blocks[i + 1][0]
+   if b + 1 < a :
+      n += a - (b + 1)
+
+b = blocks[-1][1]
+a = M
+if b + 1 < a :
+   n += a - (b + 1)
+
+print n
